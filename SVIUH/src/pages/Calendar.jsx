10cce5019e0e647287a0d4ea255e014b64753f89
CongLinh1003 <01633007559mc@gmail.com>
2025-03-06 06:18:31 +0700

    Added Calendar jsx

--- SVIUH/src/pages/Calendar.jsx
+++ SVIUH/src/pages/Calendar.jsx
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import CalendarView from "../components/CalendarView";
 import ClassRoutine from "../components/ClassRoutine";
 
 const Calendar = () => {
+  const routineEvents = [
+    {
+      title: "OOP",
+      date: new Date(2025, 2, 1),
+    },
+    {
+      title: "NOW",
+      date: new Date(2025, 2, 3), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 3, 3), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 3, 7), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 4, 3), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 4, 8), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 4, 15), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 5, 3), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 5, 9), 
+    },{
+      title: "NOW",
+      date: new Date(2025, 5, 13), 
+    },
+  ];
+
+  // Manage the current date state in the parent
+  const [currentDate, setCurrentDate] = useState(new Date());
+
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
@@ -12,8 +51,12 @@ const Calendar = () => {
         <Header />
 
         <div className="flex mt-4 px-6">
-          <ClassRoutine />
-          <CalendarView />
+          <ClassRoutine currentDate={currentDate} routineEvents={routineEvents} />
+          <CalendarView
+            currentDate={currentDate}
+            setCurrentDate={setCurrentDate}
+            routineEvents={routineEvents}
+          />
         </div>
       </div>
     </div>
